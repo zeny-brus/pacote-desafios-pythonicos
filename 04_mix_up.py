426570ddfc18fb69c398164c93f7e31066c165a8
gustavo <gustavosoares.nh@gmail.com>
2020-11-13 20:57:18 -0200

    Quarta questao resolvida

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -13,8 +13,9 @@ Assuma que a e b tem tamanho 2 ou maior.
 """
 
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    # +++ SUA SOLUÇÃO +++  
+    return 'string curta' if len(a) < 2  or len(b) < 2 else ' '.join((a.replace(a[:2], b[:2]),b.replace(b[:2], a[:2]))) 
+
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -42,3 +43,4 @@ if __name__ == '__main__':
     test(mix_up, ('dog', 'dinner'), 'dig donner')
     test(mix_up, ('gnash', 'sport'), 'spash gnort')
     test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
+    test(mix_up, ('gutto', 'f'), 'string curta')
